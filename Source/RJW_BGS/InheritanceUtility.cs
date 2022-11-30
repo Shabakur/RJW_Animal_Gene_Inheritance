2cdde450a49c0e992741c8b2892b5fc049f35bff
Shabakur <unkwownsecret@gmail.com>
2022-11-30 14:26:47 +0100

    Actually changed animal genes selection.

--- Source/RJW_BGS/InheritanceUtility.cs
+++ Source/RJW_BGS/InheritanceUtility.cs
@@ -12,51 +12,36 @@ namespace RJW_BGS
     {
         public static List<GeneDef> AnimalInheritedGenes(Pawn father, Pawn mother)
         {
+            //One parent must be an animal and the other must be human, so only one needs to return
             List<GeneDef> genelist = new List<GeneDef>();
             if (father != null && !father.RaceProps.Humanlike)
             {
-                PawnKindDef pawnKindDef = father.kindDef;
-                RaceGeneDef raceGeneDef = RJWcopy.GetRaceGenDefInternal(pawnKindDef);
-                if (raceGeneDef != null)
-                {
-                    GeneDef gene = null;
-                    //In case you hit a modded gene not currently active try again.
-                    for (int i = 0; i < 50 || gene == null; i++)
-                    {
-                        if (raceGeneDef.genes.Any())
-                        {
-                          gene = DefDatabase<GeneDef>.GetNamed(raceGeneDef.genes.RandomElement());
-                        }
-                    }
-                    if (gene != null)
-                    {
-                        genelist.Add(gene);
-                    }
-                }
+                return SelectGenes(father);
             }
 
             if (mother != null && !mother.RaceProps.Humanlike)
             {
-                PawnKindDef pawnKindDef = mother.kindDef;
-                RaceGeneDef raceGeneDef = RJWcopy.GetRaceGenDefInternal(pawnKindDef);
-                if (raceGeneDef != null)
-                {
-                    GeneDef gene = null;
+                return SelectGenes(mother);
+                //PawnKindDef pawnKindDef = mother.kindDef;
+                //RaceGeneDef raceGeneDef = RJWcopy.GetRaceGenDefInternal(pawnKindDef);
+                //if (raceGeneDef != null)
+                //{
+                //    GeneDef gene = null;
                     //In case you hit a modded gene not currently active try again.
-                    for (int i = 0; i < 50 || gene == null; i++)
-                    {
-                        if (raceGeneDef.genes.Any())
-                        {
-                            gene = DefDatabase<GeneDef>.GetNamed(raceGeneDef.genes.RandomElement());
-                        }
-                    }
-                    if (gene != null)
-                    {
-                        genelist.Add(gene);
-
-                    }
-                    
-                }
+                //    for (int i = 0; i < 50 || gene == null; i++)
+                //    {
+                //        if (raceGeneDef.genes.Any())
+                //        {
+                //            gene = DefDatabase<GeneDef>.GetNamed(raceGeneDef.genes.RandomElement());
+                //        }
+                //    }
+                //    if (gene != null)
+                //    {
+                //        genelist.Add(gene);
+//
+  //                  }
+    //                
+      //          }
             }
             return genelist;
         }
